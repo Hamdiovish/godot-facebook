2273128ba87efa670374e05289104811a1eb51bb
Hamdy BENSALAH <Hamdiovish@users.noreply.github.com>
2020-12-09 04:43:20 +0100

    Update register_types.cpp

--- GodotFacebook/register_types.cpp
+++ GodotFacebook/register_types.cpp
@@ -1,11 +1,12 @@
+#include <core/version_generated.gen.h>
 #include "register_types.h"
-#if defined(__APPLE__)
+#include <core/class_db.h>
+#include <core/engine.h>
 #include "ios/Facebook.h"
-#endif
 
 void register_GodotFacebook_types() {
 #if defined(__APPLE__)
-	ClassDB::register_class<GodotFacebook>();
+    Engine::get_singleton()->add_singleton(Engine::Singleton("GodotFacebook", memnew(GodotFacebook)));
 #endif
 }
 
